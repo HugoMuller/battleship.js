fdb1a04fadf808c8e702c5ddfbb4183ed916541a
HugoMuller <hugo.muller@free.fr>
2014-12-15 23:56:05 +0100

    Lang factory: remove downloadLanguage() as it is not used

--- public/js/services/lang.js
+++ public/js/services/lang.js
@@ -2,21 +2,17 @@
 
 //languages service
 angular.module('bs.lang').factory('Lang', ['$http', 'Global', function($http, Global){
-  this.downloadLanguage = function(lang){
-    $http
-      .get('/getLanguage/' + lang)
-      .success(function(newLang){
-        Global.lang = newLang;
-      })
-      .error(function(data, status){
-        console.error('Error', status, ': Could not load language', lang);
-      });
-  };
-
   this.setLanguageTo = function(lang){
     lang = lang || 'en';
     if(!Global.lang || Global.lang.__lang !== lang){
-      this.downloadLanguage(lang);
+      $http
+        .get('/getLanguage/' + lang)
+        .success(function(newLang){
+          Global.lang = newLang;
+        })
+        .error(function(data, status){
+          console.error('Error', status, ': Could not load language', lang);
+        });
     }
   };
   
